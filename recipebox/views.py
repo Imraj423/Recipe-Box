f364f9a38457ae4d4a754bc5e37a410e415f6b33
Imraj423 <dahqniss@gmail.com>
2020-04-01 18:50:20 -0400

    get favorite working

--- recipebox/views.py
+++ recipebox/views.py
@@ -118,7 +118,16 @@ def logout_view(request):
     return HttpResponseRedirect(reverse('homepage'))
 
 
-@allowed_users(allowed_roles=['admin', 'author'])
+user_login_required = user_passes_test(
+    lambda user: user.is_active, login_url='/')
+
+
+def active_user_required(view_func):
+    decorated_view_func = login_required(user_login_required(view_func))
+    return decorated_view_func
+
+
+@active_user_required
 def edit_recipe(request, id):
     html = 'editRecipe.html'
     instance = Recipe.objects.get(id=id)
